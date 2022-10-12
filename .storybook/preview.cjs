3b1e85ac8ec92eff1be0bf5a86851c929ce620f4
Gabriel Cedran <gabriel@getfuzey.com>
2022-10-12 21:06:30 +0100

    Add missing configuration to integration tailwind with storybook

--- .storybook/preview.cjs
+++ .storybook/preview.cjs
@@ -1,6 +1,10 @@
 
 import {themes} from '@storybook/theming'
 
+// we need to import tailwind's default configuration into storybook's preview environment, otherwise 
+// all the stylization made via tailwind won't take effect.
+import '../src/styles/global.css'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
